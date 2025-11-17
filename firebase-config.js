3aa521144688f019a71e589fe46a84a11b4a3f71
GugaToca <gustavotocalino08@outlook.com>
2025-11-17 11:55:20 -0300

    Refactor Firebase config for compatibility mode
    Updated Firebase initialization to use compatibility mode and added Firestore and Auth services.

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,18 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDgmiMtSEPcTqdfDgLOL3pzCeOL6CGaHvk",
-    authDomain: "sistemafinanceiro-c5f6b.firebaseapp.com",
-    projectId: "sistemafinanceiro-c5f6b",
-    storageBucket: "sistemafinanceiro-c5f6b.firebasestorage.app",
-    messagingSenderId: "511053597442",
-    appId: "1:511053597442:web:386d029a5432379813d01b"
-  };
+// Configuração do seu Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDgmiMtSEPcTqdfDgLOL3pzCeOL6CGaHvk",
+  authDomain: "sistemafinanceiro-c5f6b.firebaseapp.com",
+  projectId: "sistemafinanceiro-c5f6b",
+  storageBucket: "sistemafinanceiro-c5f6b.firebasestorage.app",
+  messagingSenderId: "511053597442",
+  appId: "1:511053597442:web:386d029a5432379813d01b"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Inicializa usando COMPAT
+firebase.initializeApp(firebaseConfig);
+
+// Serviços compatíveis
+const auth = firebase.auth();
+const db = firebase.firestore();
